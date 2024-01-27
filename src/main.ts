a072248a39aa9b2a1ea38d6215b60f0c499249f0
Vannyou Tang <vannyoutang23@gmail.com>
2024-01-27 16:01:59 +0700

    chore: :rocket: add simple functional middleware

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import { logger } from './logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
