b7d185d0e1e61796f99e5ae262c553b032720d47
Vannyou Tang <vannyoutang23@gmail.com>
2024-01-27 17:58:47 +0700

    chore: :rocket: add custom http exception filter

---
+++ src/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpException,
+  Catch,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const _request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    /**
+     * TODO: Dynamic code and trace_id
+     */
+    response.status(status).json({
+      code: 'E001',
+      message: exception.message,
+      timestamp: new Date().getTime(),
+      trace_id: 'TRACE_ID',
+    });
+  }
+}
