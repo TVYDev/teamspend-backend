494109e1b836976376d787647ccd7d22fe16e853
Vannyou Tang <vannyoutang23@gmail.com>
2024-04-20 17:27:08 +0700

    chore: :rocket: added config module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,16 @@ export class AppController {
 
   @Get()
   async getHello(): Promise<string> {
+    return this.appService.getHello();
+  }
+
+  @Get('/timeout')
+  async getHelloTimeout(): Promise<string> {
     /** TESTING Timeout response */
     return new Promise((resolve, _reject) => {
       setTimeout(() => {
         resolve(this.appService.getHello());
-      }, 2000);
+      }, 5000);
     });
   }
 }
