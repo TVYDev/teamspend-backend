add9c4c773ceb5136033648244e729c71d1e42ae
Vannyou Tang <vannyoutang23@gmail.com>
2024-04-28 23:26:19 +0700

    chore(auth): :rocket: define model user

---
+++ prisma/migrations/20240428161222_update_type_varchar_255_in_table_user/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `id` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `first_name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `last_name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `middle_name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `password` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `username` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "first_name" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "last_name" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "middle_name" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "password" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "username" SET DATA TYPE VARCHAR(255),
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
